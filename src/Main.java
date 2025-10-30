9e9684bb538ba74c9cc2349c67a78218034e9fc4
Francisco Javier Rodríguez Suarez <a25franciscojrs@teis.intranet>
2025-10-30 20:05:08 +0100

    Primer dia

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,38 @@ public class Main {
 
     public static int[][] movimientoCaballo(int fila, int columna) {
         int[][] tablero = new int[8][8];
+
+        int[][] movimientos = {
+                {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
+                {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
+        };
+
+        for (int[] movimiento : movimientos) {
+            int nuevaFila = fila + movimiento[0];
+            int nuevaColumna = columna + movimiento[1];
+
+            if (nuevaFila >= 0 && nuevaFila < 8 && nuevaColumna >= 0 && nuevaColumna < 8) {
+                tablero[nuevaFila][nuevaColumna] = 1;
+            }
+        }
+
         return tablero;
+
+
     }
 
     public static int[][] movimientoAlfil(int fila, int columna) {
         int[][] tablero = new int[8][8];
+
+        for (int i = fila - 1, j = columna - 1; i >= 0 && j >= 0; i--, j--) {
+            tablero[i][j] = 1;
+        }
+
+        for (int i = fila - 1, j = columna + 1; i >= 0 && j < 8; i--, j++) {
+            tablero[i][j] = 1;
+        }
+        
+
         return tablero;
     }
 
